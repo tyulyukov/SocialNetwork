95f608885bf218ec4def40978b2e38357c2b4b73
tyulyukov <makstyulyukov@gmail.com>
2022-02-22 02:19:41 +0200

    My profile page

---
+++ SocialNetwork/Controllers/MyProfileController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Controllers
+{
+    [Authorize]
+    public class MyProfileController : Controller
+    {
+        private readonly UserManager<Profile> _userManager;
+        private readonly SignInManager<Profile> _signInManager;
+
+        public MyProfileController(
+            UserManager<Profile> userManager,
+            SignInManager<Profile> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (!_signInManager.IsSignedIn(User))
+                return RedirectToPage("/Account/AccessDenied", new { Area = "Identity" });
+
+            return View(await _userManager.GetUserAsync(User));
+        }
+    }
+}
